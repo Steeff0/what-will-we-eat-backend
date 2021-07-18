ac21e2c44dc1bac84f4b5785b6ce1ac1ed7f4adf
Steven Gerritsen <steven.gerritsen@gmail.com>
2021-07-18 13:24:50 +0200

    Project setup

---
+++ src/main/java/com/hardrockdevops/whatwillweeatservices/WhatWillWeEatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hardrockdevops.whatwillweeatservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatWillWeEatServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatWillWeEatServiceApplication.class, args);
+    }
+
+}
