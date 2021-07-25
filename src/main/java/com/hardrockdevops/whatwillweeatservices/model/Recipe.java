73a818fcc6b4bc415f29892c81e3f554ef29665b
Steven Gerritsen <steven.gerritsen@gmail.com>
2021-07-25 15:01:17 +0200

    Work with database

--- src/main/java/com/hardrockdevops/whatwillweeatservices/model/Recipe.java
+++ src/main/java/com/hardrockdevops/whatwillweeatservices/model/Recipe.java
@@ -1,7 +1,31 @@
 package com.hardrockdevops.whatwillweeatservices.model;
 
-//@getter
-//@setter
-//@entity
-public class Recipe {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.io.Serializable;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class Recipe implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String Name;
+
+    private String ingredients;
+
+    private String recept;
+
+    private String author;
+
+    private int duration;
+
 }
--- src/test/java/com/hardrockdevops/whatwillweeatservices/WhatWillWeEatServiceApplicationTests.java
+++ src/test/java/com/hardrockdevops/whatwillweeatservices/WhatWillWeEatServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.hardrockdevops.whatwillweeatservices;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WhatWillWeEatServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.hardrockdevops.whatwillweeatservices;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WhatWillWeEatServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
